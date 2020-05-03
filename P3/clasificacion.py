0a339fd70165338ba1ac457500539bbee5deeab4
PATRICIA <patriciacorhid@gmail.com>
2020-05-03 18:14:22 +0200

    Regularización hecho y problemas con cross-validation

--- P3/clasificacion.py
+++ P3/clasificacion.py
@@ -113,44 +113,163 @@ def rl_sgd(x, y, lr, max_iters, tam_minibatch):
                         
         return w_list
 
-#------------------SELECCION DEL MODELO--------------------------
-#Intruduce características al modelo para ajuste polinómico
-#n: grado del polinomio, x: datos
-def modelo(x, n):
-        if n < 4:
-                poly = proc.PolynomialFeatures(n)
-                x = poly.fit_transform(x)
-        else:
-                #Si n es muy grande, solo multiplica unas características con otras, no tiene términos cuadráticos
-                poly = proc.PolynomialFeatures(degree=n, interaction_only = True)
-                x = poly.fit_transform(x)
-        return x
+def softmax(x,y,w):
 
-#------------------VALIDACIÓN--------------------------
+        i = np.random.randint(0, len(x))
+        elem = x[i]
 
-def validacion_cruzada(x, y):
-        e_cv = 0
+        print("La etiqueta es: " + str(y[i]))
+
+        suma = 0
+        for i in range(0, len(w)):
+                suma += m.exp(np.dot(w[i], elem))
+
+        p = []
+
+        for i in range(0, len(w)):
+                p.append(m.exp(np.dot(w[i], elem))/suma)
+
+        p = np.array(p)
+
+        et = np.argmax(p)
+        prob = np.amax(p)
+
+        print("La etiqueta que predice es: " + str(et))
+        print("Con probabilidad: " + str(prob))
+
+#------------------MÉTRICA DE ERROR--------------------------
+
+def accuracy(x,y,w):
+
+        accuracy = 0
         for i in range(0, len(x)):
-                x = x[i]  #Valores que uso para la validación
-                y = y[i]
-                x_val = np.delete(x,i) #Valores que uso para entrenar
-                y_val = np.delete(y,i)
-                vini = np.array([]) #Vector inicial para PLA
 
-                #Inicializo el vector inicial a 0
-                for i in range(0, len(x[0])):
-                        np.append(vini, 0.0)
+                #print("La etiqueta es: " + str(y[i]))
 
-                #Ajusto el modelo
-                w = PLA_pocket(x_val, y_val, 100, vini)
-                e_val = Err(x,y,w) #Calculo el error con el dato de validación
-                e_cv += e_val #Error acumulado
+                #Calculo el denominador de P(Cj|x) de SOFTMAX
+                suma = 0
+                for k in range(0, len(w)):
+                        suma += m.exp(np.dot(w[k], x[i]))
 
-        return e_cv/len(x)
+                #Añado en una lista las probabilidades de que el
+                #elemento de x pertenezca a cada clase
+                p = []
+                for k in range(0, len(w)):
+                        p.append(m.exp(np.dot(w[k], x[i]))/suma)
+
+                p = np.array(p)
+
+                #Su etiqueta es la clase con mayor probabilidad de pertenencia
+                et = np.argmax(p)
+
+                #Calculo el total de elementos acertados
+                if int(et) == int(y[i]):
+                        accuracy += 1
+                        
+                #prob = np.amax(p)
+                #print("La etiqueta que predice es: " + str(et))
+                #print("Con probabilidad: " + str(prob))
+
+        return accuracy/len(x)
 
 #------------------REGULARIZACIÓN--------------------------
 
+#Error regularización multietiqueta
+def Err_reg(x,y,w):
+        error = 0
+        
+        for i in range(0, len(x)):
+                #Calculo el error total como el numero de elementos mal clasificados
+                for j in range(0, len(w)):
+                        error = error - y[i][j]*m.log(sigma(np.dot(w[j],x[i])))
+        return error/len(x) + np.linalg.norm(w)**2
 
+
+# Regresion Logistica con regularización
+def rl_reg(x, y, lr, max_iters, tam_minibatch, landa):
+
+        w_list = [] #lista con todos los w de la RL
+        #Hay que hacer RL binaria 10 veces.
+        for k in range(0,10):
+                                                   
+                #Inicializa el punto inicial a (0,0)
+                w = [0 for i in range(0, len(x[0]))]
+                w = np.array(w, np.float64)
+
+                iters = 0
+                #Paro cuando ||w^t - w^(t-1)||<0.01
+                while iters < max_iters:
+                
+                        #Elijo índices para formar el minibatch
+                        m = np.random.randint(0, len(x), size=tam_minibatch)
+                        
+                        #Creo el minibatch
+                        minibatch_x = []
+                        minibatch_y = []
+                        for i in range(0, tam_minibatch):
+                                minibatch_x.append(x[m[i]])
+                                minibatch_y.append(y[m[i]])
+
+                        #Algoritmo de Gradiente descendente
+                        for i in range(0, tam_minibatch):
+
+                                #Actualiazción de w según el algoritmo
+                                suma = np.array([0 for i in range(0, len(x[0]))])
+                                for n in range(0, tam_minibatch):
+                                        #print((sigma(np.dot(w,x[n])) - y[n][k])*x[n])
+                                        aux = np.array((sigma(np.dot(w,minibatch_x[n])) - minibatch_y[n][k])*minibatch_x[n])
+                                        suma = suma + aux
+
+                                w = w - lr*(suma + landa*w)
+                        
+                        iters += 1
+
+                w_list.append(w)
+                print(iters)
+                        
+        return w_list
+
+
+#------------------VALIDACIÓN--------------------------
+
+def validacion_cruzada(x, y, n, lr, max_iters, tam_minibatch):
+        e_cv = 0
+
+        #Hago validación cruzada con n conjuntos diferentes 
+        l = len(x)
+        l = int(l/n)
+
+        datos = []
+        im_datos = []
+        datos_val = []
+        im_datos_val = []
+        for i in range(0,n):
+                print(i*l)
+                print((i+1)*l-1)
+                aux_x = np.delete(x, [i*l, (i+1)*l-1], 0)
+                aux_y = np.delete(x, [i*l, (i+1)*l-1], 0)
+                print("Aux_x: " + str(len(aux_x)))
+                datos.append(aux_x) #Valores de entrenamiento
+                im_datos.append(aux_y)
+                datos_val.append(x[i*l:(i+1)*l]) #Valores de validación
+                im_datos_val.append(y[i*l:(i+1)*l])
+
+        datos = np.array(datos)
+        im_datos = np.array(im_datos)
+
+        print(len(x))
+        print(n)
+        print(l)
+
+        #Ajusto el modelo
+        for i in range(0,n):
+                print("Datos: " + str(len(datos[i])))
+                print(len(im_datos[i]))
+                w = rl_sgd(datos[i], im_datos[i], lr, max_iters, tam_minibatch)
+                e_val = Err(datos_val[i],im_datos_val[i],w) #Calculo el error con los datos de validación
+                e_cv += e_val #Error acumulado
+
+        return e_cv/n
 
 #------------------GRAFICAS--------------------------
 def grafica(datos, im_datos, titulo):
@@ -249,22 +368,23 @@ def grafica(datos, im_datos, titulo):
 #------------------PRACTICA 3--------------------------
 
 #Leemos los datos del fichero
-x_train, y_train = readData("datos/optdigits.tra")
+x, y = readData("datos/optdigits.tra")
 x_test, y_test = readData("datos/optdigits.tes")
 
 #Establezco la longitud del conjunto de validación 
-l = len(x_train)
+l = len(x)
 l = l/4
 l = int(l)
 
-#Actualizo conjunto test y conjunto de validación
-x_train = x_train[:-l]
-y_train = y_train[:-l]
-x_validation = x_train[-l:]
-y_validation = y_train[-l:]
+#Actualizo conjunto train y conjunto de validación
+x_train = x[:-l]
+y_train = y[:-l]
+x_validation = x[-l:]
+y_validation = y[-l:]
+
+#CARACTERÍSTICAS LINEALES
 
 #Preprocesado de datos con PCA
-
 #Conjunto training
 #Se queda con las características que explican el 99% de la distribución.
 pca = PCA(n_components = 0.99, random_state=1) 
@@ -282,6 +402,10 @@ x_validation = scaler.transform(x_validation)
 x_test = pca.transform(x_test)
 x_test = scaler.transform(x_test)
 
+#Le aplico la misma transformación al archivo de train (para validar despés)
+x = pca.transform(x)
+x = scaler.transform(x)
+
 #Graficas
 #grafica(x_train, y_train, "Dígitos")
 #grafica(x_validation, y_validation, "Dígitos")
@@ -290,9 +414,11 @@ x_test = scaler.transform(x_test)
 y_train_v = formato_im(y_train)
 y_val_v = formato_im(y_validation)
 y_test_v = formato_im(y_test)
+y_v = formato_im(y)
 
+'''
 #Calculo w por medio de la regresión logística
-w = rl_sgd(x_train, y_train_v, 0.005, 50, 32)
+w = rl_sgd(x_train, y_train_v, 0.005, 100, 32)
 
 #Error de los 3 conjuntos diferentes
 e_tra = Err(x_train,y_train_v, w)
@@ -303,65 +429,83 @@ print("El error del conjunto de entrenamiento es: " + str(e_tra))
 print("El error del conjunto de validación es: " + str(e_val))
 print("El error del conjunto test es: " + str(e_test))
 
-def pruebecilla(x,y,w):
-
-        i = np.random.randint(0, len(x))
-        elem = x[i]
-
-        print("La etiqueta es: " + str(y[i]))
-
-        suma = 0
-        for i in range(0, len(w)):
-                suma += m.exp(np.dot(w[i], elem))
-
-        p = []
-
-        for i in range(0, len(w)):
-                p.append(m.exp(np.dot(w[i], elem))/suma)
-
-        p = np.array(p)
-
-        et = np.argmax(p)
-        prob = np.amax(p)
-
-        print("La etiqueta que predice es: " + str(et))
-        print("Con probabilidad: " + str(prob))
-
-def accuracy(x,y,w):
-
-        accuracy = 0
-        for i in range(0, len(x)):
-
-                #print("La etiqueta es: " + str(y[i]))
-
-                #Calculo el denominador de P(Cj|x) de SOFTMAX
-                suma = 0
-                for k in range(0, len(w)):
-                        suma += m.exp(np.dot(w[k], x[i]))
-
-                #Añado en una lista las probabilidades de que el
-                #elemento de x pertenezca a cada clase
-                p = []
-                for k in range(0, len(w)):
-                        p.append(m.exp(np.dot(w[k], x[i]))/suma)
-
-                p = np.array(p)
-
-                #Su etiqueta es la clase con mayor probabilidad de pertenencia
-                et = np.argmax(p)
-
-                #Calculo el total de elementos acertados
-                if int(et) == int(y[i]):
-                        accuracy += 1
-                        
-                #prob = np.amax(p)
-                #print("La etiqueta que predice es: " + str(et))
-                #print("Con probabilidad: " + str(prob))
-
-        return accuracy/len(x)
-
-#Prueba de lo que devuelve
-for i in range(0,5):
-       pruebecilla(x_train,y_train,w)
 
 print("La precisión es de: " + str(accuracy(x_train,y_train,w)))
+print("La precisión es de: " + str(accuracy(x_validation,y_validation,w)))
+'''
+
+#CARACTERÍSTICAS CUADRÁTICAS:
+
+#Añado características cuadráticas a los datos
+poly = proc.PolynomialFeatures(2)
+x_train = poly.fit_transform(x_train)
+x_validation = poly.transform(x_validation)
+x_test = poly.transform(x_test)
+x = poly.transform(x)
+
+#Preprocesado de datos con PCA
+#Conjunto training
+#Se queda con las características que explican el 99% de la distribución.
+pca = PCA(n_components = 0.99, random_state=1) 
+pca.fit(x_train)
+x_train = pca.transform(x_train)
+#Escala los datos
+scaler = proc.StandardScaler().fit(x_train)
+x_train = scaler.transform(x_train)
+
+#Le aplico la misma transformación al conjunto de validación
+x_validation = pca.transform(x_validation)
+x_validation = scaler.transform(x_validation)
+
+#Le aplico la misma transformación al conjunto test
+x_test = pca.transform(x_test)
+x_test = scaler.transform(x_test)
+
+#Le aplico la misma transformación al archivo de train (para validar despés)
+x = pca.transform(x)
+x = scaler.transform(x)
+
+'''
+#Calculo w por medio de la regresión logística
+w = rl_sgd(x_train, y_train_v, 0.005, 100, 32)
+
+#Error de los 3 conjuntos diferentes
+e_tra = Err(x_train,y_train_v, w)
+e_val = Err(x_validation, y_val_v, w)
+e_test = Err(x_test, y_test_v, w)
+
+print("El error del conjunto de entrenamiento es: " + str(e_tra))
+print("El error del conjunto de validación es: " + str(e_val))
+print("El error del conjunto test es: " + str(e_test))
+
+print("La precisión es de: " + str(accuracy(x_train,y_train,w)))
+print("La precisión es de: " + str(accuracy(x_validation,y_validation,w)))
+'''
+
+#MODELO CUADRÁTICO REGULARIZADO 
+'''
+#Calculo w por medio de la regresión logística
+w = rl_reg(x_train, y_train_v, 0.005, 100, 32, 0.0001)
+
+#Error de los 3 conjuntos diferentes
+e_tra = Err_reg(x_train,y_train_v, w)
+e_val = Err_reg(x_validation, y_val_v, w)
+e_test = Err_reg(x_test, y_test_v, w)
+
+print("El error del conjunto de entrenamiento es: " + str(e_tra))
+print("El error del conjunto de validación es: " + str(e_val))
+print("El error del conjunto test es: " + str(e_test))
+
+print("La precisión es de: " + str(accuracy(x_train,y_train,w)))
+print("La precisión es de: " + str(accuracy(x_validation,y_validation,w)))
+'''
+
+#CALCULO AJUSTE CON MODELO ELEGIDO
+#w = rl_sgd(x, y_v, 0.005, 100, 32)
+#e = Err(x,y_v, w)
+#print("El error del conjunto de entrenamiento es: " + str(e))
+#print("La precisión es de: " + str(accuracy(x,y,w)))
+
+e_cv = validacion_cruzada(x, y_v, 2, 0.005, 100, 32)
+print("El error de validación cruzada es: " + str(e_cv))
+
